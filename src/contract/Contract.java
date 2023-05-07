30c04b6f3a728935109c1c8c1370e31179ad2336
kbj1570 <playstudy00@gmail.com>
2023-05-08 08:58:20 +0900

    Contract 생성

--- src/contract/Contract.java
+++ src/contract/Contract.java
@@ -14,23 +14,30 @@ public class Contract {
 	private int insuranceFee;
 	private boolean isExpired;
 	private boolean renewalStatus;
-	public Contract(){
+	public Contract(int insuranceID, int insuranceFee, Date expirationDate, String coverageDetails){
+		this.insuraceID = insuranceID;
+		this.insuranceFee = insuranceFee;
+		this.expirationDate = expirationDate;
+		this.coverageDetails = coverageDetails;
 
 	}
 
 	public boolean checkExpired(){
 		int result = expirationDate.compareTo(new Date());
 
+
 		if(result <= 0){
-			return false; // 만료안됨
+			isExpired = false; // 만료 안됨
 		}
 		else{
-			return true;// 만료됨
+			isExpired = true; // 만료됨
 		}
+		return isExpired;
 	}
 
-	public boolean updateRenuewalStatus(){
-		return false;
+	public boolean updateRenuewalStatus(boolean renewalStatus){
+		this.renewalStatus = renewalStatus;
+		return this.renewalStatus;
 	}
 
 }
