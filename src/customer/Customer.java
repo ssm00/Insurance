19926f836cdb41b48059572fad393463257cafdb
kbj1570 <playstudy00@gmail.com>
2023-05-08 10:03:04 +0900

    CustomerList 추가

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -1,17 +1,20 @@
 package customer;
 
 public class Customer {
-
-	private String account;
-	private String address;
-	private int age;
 	private String customerID;
 	private String customerName;
+	private String address;
+	private int age;
 	private String gender;
 	private String job;
 
-	public Customer(){
-
+	public Customer(String customerID, String customerName, String address, int age, String gender, String job){
+		this.customerID = customerID;
+		this.customerName = customerName;
+		this.address = address;
+		this.age = age;
+		this.gender = gender;
+		this.job = job;
 	}
 
 	public void finalize() throws Throwable {
